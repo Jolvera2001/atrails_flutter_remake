f98462df83734e69e9ae4b8ec3c868982883aff1
Jolvera2001 <jolvera6@stedwards.edu>
2024-06-11 22:14:41 -0500

    added riverpod dependencies and firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'Pages/start_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // making sure that widgets are initialized before running
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
